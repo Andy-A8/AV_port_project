bffd5f105b7067d6adbe3b5f778f790ceec61964
Andy-A8 <aaallotey006@st.ug.edu.gh>
2025-01-18 19:00:49 +0000

    Added middleware for json parsing

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/index');
 const apiRoutes = require('./routes/api');
+const bodyParser = require('body-parse');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 app.use('/api', apiRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Start Server(api.js)
 const PORT = process.env.PORT || 5000;
