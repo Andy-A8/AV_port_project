38add8613d2ca4c1d60a1f4bc8a620bd7bedebef
Andy-A8 <aaallotey006@st.ug.edu.gh>
2025-01-27 15:30:02 +0000

    Added middleware for api algorithm routes

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
+const algorithmRoutes = require('./routes/algorithmRoutes');
 require('dotenv').config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Middleware for API routes
 app.use('/api', routes);
+app.use('/api/algorithms', algorithmRoutes);
 
 // Routes
 app.get('/', (req, res) => {
