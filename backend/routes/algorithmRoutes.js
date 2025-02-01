b025a0ee8a99261eb07a76b46675e64aa278ffad
Andy-A8 <aaallotey006@st.ug.edu.gh>
2025-02-01 15:24:09 +0000

    Updated sorting routes to call appropriate function

--- backend/routes/algorithmRoutes.js
+++ backend/routes/algorithmRoutes.js
@@ -4,16 +4,22 @@ const { validateArray } = require('../middleware/validate_input');
 const { bubbleSort, quickSort, mergeSort } = require('../services/sortingService');
 
 // Sorting Routes
-router.post('/sort/bubble', validateArray, (req, res) => {
-  res.send('Bubble sort logic will go here.');
+router.post('/sort/bubble', (req, res) => {
+  const { array } = req.body;
+  const result = bubbleSort(array);
+  res.status(200).json(result);
 });
 
-router.post('/sort/quick', validateArray, (req, res) => {
-  res.send('Quick sort logic will go here.');
+router.post('/sort/quick', (req, res) => {
+  const { array } = req.body;
+  const result = quickSort(array);
+  res.status(200).json(result);
 });
 
-router.post('/sort/merge', validateArray, (req, res) => {
-  res.send('Merge sort logic will go here.');
+router.post('/sort/merge', (req, res) => {
+  const { array } = req.body;
+  const result = mergeSort(array);
+  res.status(200).json(result);
 });
 
 // Searching Routes
