123503cb258266c63f61cd1eb5243fd32569ba03
Andy-A8 <aaallotey006@st.ug.edu.gh>
2025-02-06 00:55:41 +0000

    Updated API responses to include steps for sorting algorithms

---
+++ backend/controllers/sortingController.js
@@ -0,0 +1,28 @@
+const AlgorithmLog = require('../models/AlgorithmLog');
+const { bubbleSort, quickSort } = require('../utils/sortingAlgorithms');
+
+exports.sortArray = async (req, res) => {
+  const { array, algorithm } = req.body;
+  let result;
+
+// Select the appropriate sorting algorithm
+  if (algorithm === 'bubble') {
+    result = bubbleSort(array);
+  } else if (algorithm == 'quick') {
+    result = quickSort(array);
+  } else {
+      return es.status(400).json({ error: "Invalid sorting algorithm" });
+  }
+
+  // Save execution log
+  const log = new AlgorithmLog({
+    algorithm,
+    input: array,
+    output: result.sortedArray,
+    steps: result.steps,
+    executionTime: `${result.steps.length} steps`
+  });
+  await log.save();
+
+  res.json(result);
+};
